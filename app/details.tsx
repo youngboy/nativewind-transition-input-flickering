00ab6d01b3a3f50199e6800cd05fdd0b37421ea3
yangbo <yangbohnzz@gmail.com>
2024-11-15 20:17:16 +0800

    add some text

--- app/details.tsx
+++ app/details.tsx
@@ -1,5 +1,5 @@
 import { Stack, useLocalSearchParams } from 'expo-router';
-import { TextInput, View } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 
 export default function Details() {
   const { name } = useLocalSearchParams();
@@ -10,6 +10,10 @@ export default function Details() {
       <View className="flex-1">
         <View className="bg-green-500 flex-1">
         </View>
+        <Text>
+        👇 try input something in the input field below,
+          noticed the input is flickering
+        </Text>
         <View className="bg-yellow-500 h-20">
           <TextInput className='flex-1' placeholder='Enter something' />
         </View>
--- app/index.tsx
+++ app/index.tsx
@@ -11,11 +11,11 @@ export default function Home() {
       <View className="flex-1">
         <View className="bg-green-500 flex-1">
           <Text className={`
-            absolute top-[100px] left-[230px]
+            absolute top-[200px] left-[230px]
             transition-all  text-white
             ${text ? 'opacity-100' : 'opacity-0'}
           `}>
-            Absolute Position Text
+            Absolute Position Text Appears
           </Text>
           <TextInput className='bg-red-500 h-20' value={text} onChangeText={setText} />
           <Link className='py-4 bg-blue-500 text-white' href="/details">Go to details</Link>
